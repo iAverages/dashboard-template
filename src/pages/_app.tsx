58387901b8c1b96233cf51f6172b0cf59aae0459
iAverages <31937542+iAverages@users.noreply.github.com>
2023-04-26 20:57:32 +0100

    Begin adding layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,33 @@
-import { type AppType } from "next/app";
+import { AppProps, type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { Fragment } from "react";
+import { NextPage } from "next";
+import GlobalLayout from "~/components/layout";
 
-const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    removeGlobalLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
+
+const MyApp: AppType<{ session: Session | null }> = ({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) => {
+    const Layout = Component.removeGlobalLayout || false ? GlobalLayout : Fragment;
+
     return (
         <SessionProvider session={session}>
-            <Component {...pageProps} />
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
         </SessionProvider>
     );
 };
