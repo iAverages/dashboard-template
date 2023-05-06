7f22367734bdf3df5a4a52e98f8dc33258f83824
iAverages <31937542+iAverages@users.noreply.github.com>
2023-05-06 14:04:52 +0100

    Make show component work with arrays

--- src/components/show.tsx
+++ src/components/show.tsx
@@ -1,10 +1,14 @@
-import { ReactElement, ReactNode } from "react";
+import { ReactElement } from "react";
+
+type MapNonNullable<T> = {
+    [K in keyof T]: NonNullable<T[K]>;
+};
 
 type ShowProps<T> = {
     when: T | undefined | null | false;
     keyed?: boolean;
     fallback?: ReactElement;
-    children: (item: NonNullable<T>) => ReactElement;
+    children: (item: T extends ReadonlyArray<any> ? MapNonNullable<T> : NonNullable<T>) => ReactElement;
 };
 
 export function Show<T>({ when, fallback, children }: ShowProps<T>) {
@@ -12,5 +16,17 @@ export function Show<T>({ when, fallback, children }: ShowProps<T>) {
         return fallback ?? null;
     }
 
-    return children(when as NonNullable<T>);
+    if (Array.isArray(when)) {
+        if (when.length === 0) {
+            return fallback ?? null;
+        }
+
+        const allTrue = when.every((item) => !!item);
+        if (allTrue) {
+            return children(when as any);
+        }
+        return fallback ?? null;
+    }
+
+    return children(when as any);
 }
