06a33f4677d6faeb38328fc43290d45c6f1ce0b5
iAverages <31937542+iAverages@users.noreply.github.com>
2023-05-06 14:05:44 +0100

    Setup navigation links

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,15 +1,60 @@
-import { Activity, CreditCard, DollarSign, Download, Users } from "lucide-react";
-import { ReactNode } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ReactNode, useCallback, useEffect } from "react";
 import TeamSwitcher from "~/components/tenantSwitcher";
 import { ThemeToggle } from "~/components/themeSwitcher";
-
-import { Button } from "~/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+import { cn } from "~/lib/utils";
 
 type NavigationProps = {
     children: ReactNode;
 };
+
+type NavLinkProps = {
+    name: string;
+    href: string;
+};
+
+const NavLink = ({ href, name }: NavLinkProps) => {
+    const { pathname, query } = useRouter();
+
+    const getUrl = useCallback((url: string) => {
+        const urlSplit = url.split("/");
+        urlSplit.shift();
+        urlSplit.shift();
+        return "/" + urlSplit.join("/");
+    }, []);
+
+    useEffect(() => {
+        console.log(getUrl(pathname), getUrl(pathname) === href, getUrl(pathname).startsWith(href));
+    }, [pathname, href]);
+
+    return (
+        <Link
+            href={`/${query.tenantId}/${href}`}
+            className={cn(
+                "transition-colors hover:text-foreground/80",
+                getUrl(pathname).startsWith(href) ? "text-foreground" : "text-foreground/60"
+            )}
+        >
+            {name}
+        </Link>
+    );
+};
+
+const links: NavLinkProps[] = [
+    {
+        href: "/dashboard",
+        name: "Dashboard",
+    },
+    {
+        href: "/users",
+        name: "Manage Users",
+    },
+    {
+        href: "/settings",
+        name: "Settings",
+    },
+];
 
 const Navigation = ({ children }: NavigationProps) => {
     return (
@@ -17,10 +62,14 @@ const Navigation = ({ children }: NavigationProps) => {
             <div className="border-b">
                 <div className="flex h-16 items-center px-4">
                     <TeamSwitcher />
-                    {/* <MainNav className="mx-6" /> */}
+                    <div className="mr-4 hidden md:flex">
+                        <nav className="flex items-center space-x-6 text-sm font-medium">
+                            {links.map((link, idx) => (
+                                <NavLink key={`${idx}-${link.href}`} {...link} />
+                            ))}
+                        </nav>
+                    </div>
                     <div className="ml-auto flex items-center space-x-4">
-                        {/* <Search />
-                        <UserNav /> */}
                         <ThemeToggle />
                     </div>
                 </div>
